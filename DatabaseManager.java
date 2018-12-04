9cbb94a2de6f3be72e874401205f389d1172427b
Jacky Zheng <42841563+zheng416@users.noreply.github.com>
2018-12-03 21:01:39 -0500

    Update DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -1,12 +1,9 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 
 /**
  * <h1>Database Manager</h1>
- * 
+ *
  * Used to locally save and retrieve data.
  */
 public class DatabaseManager {
@@ -20,12 +17,12 @@ public class DatabaseManager {
      * <li>Maximum Carry Weight</li>
      * </ol>
      * If filePath does not exist, a blank ArrayList will be returned.
-     * 
+     *
      * @param file CSV File
      * @return ArrayList of vehicles
      */
     public static ArrayList<Vehicle> loadVehicles(File file) {
-       //TODO
+        //TODO
         ArrayList<Vehicle> automobiles = new ArrayList<Vehicle>();
         try {
             FileReader fr = new FileReader(file);
@@ -51,10 +48,10 @@ public class DatabaseManager {
         return automobiles;
     }
 
-    
-    
-    
-    
+
+
+
+
     /**
      * Creates an ArrayList of Packages from the passed CSV file. The values are in
      * the CSV file as followed:
@@ -69,14 +66,14 @@ public class DatabaseManager {
      * <li>State</li>
      * <li>ZIP Code</li>
      * </ol>
-     * 
+     *
      * If filePath does not exist, a blank ArrayList will be returned.
-     * 
+     *
      * @param file CSV File
      * @return ArrayList of packages
      */
     public static ArrayList<Package> loadPackages(File file) {
-    	//TODO
+        //TODO
         ArrayList<Package> shipment = new ArrayList<Package>();
         try {
             FileReader fr = new FileReader(file);
@@ -93,56 +90,87 @@ public class DatabaseManager {
         }
         return shipment;
     }
-    
-    
-    
-    
-    
+
+
+
+
+
 
     /**
      * Returns the total Profits from passed text file. If the file does not exist 0
      * will be returned.
-     * 
+     *
      * @param file file where profits are stored
      * @return profits from file
      */
     public static double loadProfit(File file) {
-    	//TODO
+        //TODO
+        String profit;
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            profit = br.readLine();
+        } catch (Exception e) {
+            return 0;
+        }
+        return Double.parseDouble(profit);
     }
 
-    
-    
-    
-    
+
+
+
+
     /**
      * Returns the total number of packages shipped stored in the text file. If the
      * file does not exist 0 will be returned.
-     * 
+     *
      * @param file file where number of packages shipped are stored
      * @return number of packages shipped from file
      */
     public static int loadPackagesShipped(File file) {
-    	//TODO
+        //TODO
+        String number;
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            number = br.readLine();
+        } catch (Exception e) {
+            return 0;
+        }
+        return Integer.parseInt(number);
     }
 
-    
-    
-    
+
+
+
     /**
      * Returns whether or not it was Prime Day in the previous session. If file does
      * not exist, returns false.
-     * 
+     *
      * @param file file where prime day is stored
      * @return whether or not it is prime day
      */
     public static boolean loadPrimeDay(File file) {
-    	//TODO
+        //TODO
+        String number;
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            number = br.readLine();
+        } catch (Exception e) {
+            return false;
+        }
+        if (Integer.parseInt(number) == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    
-    
-    
-    
+
+
+
+
     /**
      * Saves (writes) vehicles from ArrayList of vehicles to file in CSV format one vehicle per line.
      * Each line (vehicle) has following fields separated by comma in the same order.
@@ -151,17 +179,28 @@ public class DatabaseManager {
      * <li>Vehicle License Plate</li>
      * <li>Maximum Carry Weight</li>
      * </ol>
-     * 
+     *
      * @param file     File to write vehicles to
      * @param vehicles ArrayList of vehicles to save to file
      */
     public static void saveVehicles(File file, ArrayList<Vehicle> vehicles) {
-    	//TODO
+        //TODO
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < vehicles.size() ; i++) {
+                String writing = vehicles.get(i) + "," + vehicles.get(i).getLicensePlate() + "," + vehicles.get(i).getMaxWeight();
+                bw.write(writing);
+                bw.newLine();
+            }
+        } catch (Exception e) {
+            System.out.print("");
+        }
     }
 
-    
-    
-    
+
+
+
     /**
      * Saves (writes) packages from ArrayList of package to file in CSV format one package per line.
      * Each line (package) has following fields separated by comma in the same order.
@@ -176,57 +215,109 @@ public class DatabaseManager {
      * <li>State</li>
      * <li>ZIP Code</li>
      * </ol>
-     * 
+     *
      * @param file     File to write packages to
      * @param packages ArrayList of packages to save to file
      */
     public static void savePackages(File file, ArrayList<Package> packages) {
-    	//TODO
+        //TODO
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < packages.size() ; i++) {
+                String writing = packages.get(i).getID() + "," + packages.get(i).getProduct() + "," +
+                        packages.get(i).getWeight() + "," + packages.get(i).getPrice() + "," +
+                        packages.get(i).getDestination().getName() + "," + packages.get(i).getDestination().getAddress()
+                        + "," + packages.get(i).getDestination().getCity() + "," +
+                        packages.get(i).getDestination().getState() + "," + packages.get(i).getDestination().getZipCode();
+                bw.write(writing);
+                bw.newLine();
+            }
+        } catch (Exception e) {
+            System.out.print("");
+        }
     }
 
-    
-    
-    
+
+
+
     /**
      * Saves profit to text file.
-     * 
+     *
      * @param file   File to write profits to
      * @param profit Total profits
      */
 
     public static void saveProfit(File file, double profit) {
-    	//TODO
+        //TODO
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+
+                String writing = Double.toString(profit);
+                bw.write(writing);
+                bw.newLine();
+
+        } catch (Exception e) {
+            System.out.print("");
+        }
     }
 
-    
-    
-    
-    
+
+
+
+
     /**
      * Saves number of packages shipped to text file.
-     * 
+     *
      * @param file      File to write profits to
      * @param nPackages Number of packages shipped
      */
 
     public static void savePackagesShipped(File file, int nPackages) {
-    	//TODO
+        //TODO
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+
+            String writing = Integer.toString(nPackages);
+            bw.write(writing);
+            bw.newLine();
+
+        } catch (Exception e) {
+            System.out.print("");
+        }
     }
 
-    
-    
-    
-    
-    
+
+
+
+
+
     /**
      * Saves status of prime day to text file. If it is primeDay "1" will be
      * writtern, otherwise "0" will be written.
-     * 
+     *
      * @param file     File to write profits to
      * @param primeDay Whether or not it is Prime Day
      */
 
     public static void savePrimeDay(File file, boolean primeDay) {
-    	//TODO
+        //TODO
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+
+            int num;
+            if (primeDay) {
+                num = 1;
+            } else {
+                num = 0;
+            }
+            String writing = Integer.toString(num);
+            bw.write(writing);
+            bw.newLine();
+
+        } catch (Exception e) {
+            System.out.print("");
+        }
     }
 }
